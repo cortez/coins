af6800ceb24c95b4ef45affc098ba56a3012c7d0
Joseph Cortez <joseph@Josephs-MacBook-Pro.local>
2022-11-26 22:16:28 -0600

    updated files

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
     mongoose.connect(process.env.DB_CONNECTION);
-    // console.log("Connected to database");
+    console.log("Connected to database");
 }
 
 export default connect;
