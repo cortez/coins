76d9a2c0499e7bf51ef378365a601af12dc7a344
Joseph Cortez <joseph@Josephs-MacBook-Pro.local>
2023-01-14 09:26:51 -0600

    Committed with Git-Upload

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -94,7 +94,7 @@ export default function DynamicPage() {
         <>
           <title>{formatter.format(total)}</title>
           <meta property="og:title" content={`${slug}`} />
-          <meta property="og:image" content="https://cortez.link/a/coins-meta.png" />
+          {/* <meta property="og:image" content="https://cortez.link/a/coins-meta.png" /> */}
           <Fade cascade damping={0.1}>
               <h1 className="total-value">{<Number n={total}></Number>}</h1>
             <Link href="/">
